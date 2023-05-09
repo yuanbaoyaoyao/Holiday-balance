7ba2df8ef94207a6f875f8b62fa2aba3fc8f9353
黄俊尧 <583515921@qq.com>
2023-05-09 14:42:42 +0800

    set custom tabbar

--- miniprogram/custom-tab-bar/index.ts
+++ miniprogram/custom-tab-bar/index.ts
@@ -1,32 +1,33 @@
-Page({
-
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        active: 0,
-        tabbars: [
-            { "text": '我的假日', "iconPath": "/assets/holidays.svg", "selectedIconPath": "/assets/holidays.svg" },
-            { "text": '剩余假期', "iconPath": "/assets/remain.svg", "selectedIconPath": "/assets/remain.svg" },
-            { "text": '关于我', "iconPath": "/assets/cat.svg", "selectedIconPath": "/assets/cat.svg" }
-        ]
-    },
-    onLoad() {
-        const page: any = getCurrentPages().pop();
-        console.log(page);
-
-        this.setData({
-            // active: this.data.list.findIndex(item => item.url === `/${page.route}`)
-        });
-    },
-    /**
-       * tab点击事件
-       * @param event 
-       */
-    tabOnChange(event: any) {
-        // this.setData({ active: event.detail });  // 官方示例的这一行不要加上
-        wx.switchTab({
-            url: this.data.list[event.detail].url
-        })
-    },
+Component({
+  data: {
+    selected: null,
+    color: "#7A7E83",
+    selectedColor: "#3cc51f",
+    list: [{
+      pagePath: "/pages/legalHolidays/legalHolidays",
+      iconPath: "/assets/holidays.svg",
+      selectedIconPath: "/assets/holidays.svg",
+      text: "我的假日"
+    }, {
+      pagePath: "/pages/vacationRemain/vacationRemain",
+      iconPath: "/assets/remain.svg",
+      selectedIconPath: "/assets/remain.svg",
+      text: "假期余额"
+    }, {
+      pagePath: "/pages/about/about",
+      iconPath: "/assets/cat.svg",
+      selectedIconPath: "/assets/cat.svg",
+      text: "关于我"
+    }]
+  },
+  attached() {
+  },
+  methods: {
+    switchTab(e) {
+      console.log("e:",e.currentTarget.dataset)
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({url})
+    }
+  }
 })
