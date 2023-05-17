49078ec78c89f687c9f8d469e70f3b76b0edd3a8
黄俊尧 <583515921@qq.com>
2023-05-17 14:30:17 +0800

    set basic zongzi animation

--- miniprogram/pages/legalHolidays/legalHolidays.ts
+++ miniprogram/pages/legalHolidays/legalHolidays.ts
@@ -7,13 +7,13 @@ Page({
         holidays: [
             { name: "元旦节", date: "12月31日至1月2日", count: "3天", color: "#FFF6DD", },
             { name: "春节", date: "1月21日至1月27日", count: "3天", color: "", },
-            { name: "清明节", date: "4月5日", count: "3天", color: "#99DAC2" },
+            { name: "清明节", date: "4月5日", count: "3天", color: "#99DAC2", image: "" },
             {
                 name: "劳动节", date: "4月29日至5月3日", count: "5天", color: "#76A7FA",
             },
             {
                 name: "端午节", date: "6月22日至6月24日", count: "3天", color: "#B1C5AC",
-                image: "background-image: url('/assets/zongzi.svg');background-size: 80px 80px;background-position: right;"
+                // image: "background-image: url('/assets/zongzi.svg');background-size: 80px 80px;background-position: right;"
             },
             {
                 name: "中秋节", date: "6月22日至6月24日", count: "8天", color: "black",
@@ -44,6 +44,16 @@ Page({
         laborDayThumbsUpClass: "",
         isAnimationLaborDay: true,
         laborTimer: 0,
+        zongziResultAnimationClass: "",
+        zongziItemAnimationClass: "",
+        zongziAssetsAnimationClass: ""
+    },
+    handleMixZongZi() {
+        this.setData({
+            zongziResultAnimationClass: "loading-result-animation",
+            zongziItemAnimationClass: "loading-item-animation",
+            zongziAssetsAnimationClass:"loading-assets-animation"
+        })
     },
     handleAnimationLaborDay() {
         if (this.data.isAnimationLaborDay) {
