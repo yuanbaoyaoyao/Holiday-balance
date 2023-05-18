a1ddfb0a679e2dcfc87bb532916b198d620886ac
黄俊尧 <583515921@qq.com>
2023-05-18 22:11:33 +0800

    add handleCountHolidayArr in date.ts

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,18 +1,31 @@
 // app.ts
 App<IAppOption>({
-  globalData: {},
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    globalData: {
+        //调休日
+        compensatoryLeaveDays: ["1/28", "1/29", "4/23", "5/6", "6/25", "10/7", "10/8"],
+        holidays: [
+            //元旦比较特殊，包含上一年
+            { name: "元旦", startYear: 2022, endYear: 2023, date: "12/31-1/2", count: "3天", color: "#FFF6DD", },
+            { name: "春节", date: "1/21-1/27", count: "3天", },
+            { name: "清明节", date: "4/5-4/5", count: "1天", color: "#99DAC2" },
+            { name: "劳动节", date: "4/29-5/3", count: "5天", color: "#76A7FA", },
+            { name: "端午节", date: "6/22-6/24", count: "3天", color: "#B1C5AC", },
+            { name: "中秋节", date: "9/29-9/29", count: "1天", },
+            { name: "国庆节", date: "9/30-10/6", count: "7天", color: "#FB7A5A", },
+        ]
+    },
+    onLaunch() {
+        // 展示本地存储能力
+        const logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        console.log(res.code)
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      },
-    })
-  },
+        // 登录
+        wx.login({
+            success: res => {
+                console.log(res.code)
+                // 发送 res.code 到后台换取 openId, sessionKey, unionId
+            },
+        })
+    },
 })
--- miniprogram/pages/date/date.ts
+++ miniprogram/pages/date/date.ts
@@ -1,179 +1,208 @@
 import { getLunar } from "chinese-lunar-calendar";
 // pages/date/date.ts
+//TODO 自定义假期，可能需要globalData
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    weekdays: ["一", "二", "三", "四", "五", "六", "日",],
-    yearMonths: [],
-    datesOfYear: [],
-    today: '',
-    monthNow: '',
-    isAllShow: false,
-    loading: false
-  },
-  GetYearMonths(year, month) {
-    let fullDate = String(year) + '年' + String(month) + '月';
-    this.data.yearMonths.push(fullDate)
-  },
-  setDatesOfYear(e) {
-    this.setData({
-      loading: true
-    })
-    setTimeout(() => {
-      this.setData({
-        loading: false
-      })
-    }, 500);
-    this.data.yearMonths = []
-    this.data.datesOfYear = []
-    const year = new Date().getFullYear()
-    let datesOfYear = []
-    if (e != undefined && this.data.isAllShow == false) {
-      this.setData({ isAllShow: true })
-      let index = e.currentTarget.dataset.index + 1
-      let dates = this.SetDates(year, index)
-      datesOfYear.push(dates)
-    } else {
-      this.setData({ isAllShow: false })
-      for (let i = 1; i <= 12; i++) {
-        let dates = this.SetDates(year, i)
-        datesOfYear.push(dates)
-      }
-    }
-    this.setData({
-      datesOfYear: datesOfYear,
-      yearMonths: this.data.yearMonths
-    })
-  },
-  SetDates(year, month) {
-    this.GetYearMonths(year, month)
-    let firstWeekDay = this.GetMonthFirstWeekDay(year, month)
-    let lastDayOfMonth = this.GetMonthLastDay(year, month)
-    let cycleNumbers = (lastDayOfMonth % 7) != 0 ? parseInt(String(lastDayOfMonth / 7)) + 1 : parseInt(String(lastDayOfMonth / 7));
-    let countDays = 0
-    let datesArr = []
-    for (let i = 0; i <= cycleNumbers; i++) {
-      let tempArr = []
-      for (let j = 0; j < 7; j++) {
-        let tempArrItem = {
-          gregorian: '',
-          lunar: ''
-        }
-        if (i == 0) {
-          //firstWeekDay为0 需要特殊处理
-          if (firstWeekDay == 0) {
-            if (j < 6) {
-              tempArr.push("")
-            } else {
-              countDays++
-              tempArrItem = this.handleSetDatesItem(tempArrItem, year, month, countDays)
-              tempArr.push(tempArrItem)
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        weekdays: ["一", "二", "三", "四", "五", "六", "日",],
+        compensatoryLeaveDays: getApp().globalData.compensatoryLeaveDays,
+        holidayArr: [],
+        yearMonths: [],
+        datesOfYear: [],
+        today: '',
+        monthNow: '',
+        isAllShow: false,
+        loading: false,
+        array: 1
+    },
+    handleCountHolidayArr() {
+        let holidays = getApp().globalData.holidays
+        let holidayArr = []
+        for (let holiday of holidays) {
+            let startYear = new Date().getFullYear()
+            let endYear = new Date().getFullYear()
+            let holidayDateArr = holiday.date.split("-")
+            if (holiday.startYear != null) {
+                startYear = holiday.startYear
+                endYear = holiday.endYear
             }
-          } else {
-            if (firstWeekDay <= j + 1) {
-              countDays++
-              tempArrItem = this.handleSetDatesItem(tempArrItem, year, month, countDays)
-              tempArr.push(tempArrItem)
-            } else {
-              tempArr.push("")
+            let startDate = new Date(startYear + "/" + holidayDateArr[0]);
+            let endDate = new Date(endYear + "/" + holidayDateArr[1]);
+            while (startDate <= endDate) {
+                let date = startDate.toLocaleDateString('en-US', { month: 'numeric', day: 'numeric' });
+                holidayArr.push(date);
+                startDate.setDate(startDate.getDate() + 1);
             }
-          }
         }
-        else if (countDays < lastDayOfMonth) {
-          countDays++
-          tempArrItem = this.handleSetDatesItem(tempArrItem, year, month, countDays)
-          tempArr.push(tempArrItem)
-        } else if (countDays >= lastDayOfMonth) {
-          countDays++
-          tempArr.push("")
+        this.setData({
+            holidayArr: holidayArr
+        })
+
+    },
+    GetYearMonths(year, month) {
+        let fullDate = String(year) + '年' + String(month) + '月';
+        this.data.yearMonths.push(fullDate)
+    },
+    setDatesOfYear(e) {
+        this.setData({
+            loading: true
+        })
+        setTimeout(() => {
+            this.setData({
+                loading: false
+            })
+        }, 500);
+        this.data.yearMonths = []
+        this.data.datesOfYear = []
+        const year = new Date().getFullYear()
+        let datesOfYear = []
+        if (e != undefined && this.data.isAllShow == false) {
+            this.setData({ isAllShow: true })
+            let index = e.currentTarget.dataset.index + 1
+            let dates = this.SetDates(year, index)
+            datesOfYear.push(dates)
+        } else {
+            this.setData({ isAllShow: false })
+            for (let i = 1; i <= 12; i++) {
+                let dates = this.SetDates(year, i)
+                datesOfYear.push(dates)
+            }
         }
-      }
-      datesArr.push(tempArr)
+        this.setData({
+            datesOfYear: datesOfYear,
+            yearMonths: this.data.yearMonths
+        })
+    },
+    SetDates(year, month) {
+        this.GetYearMonths(year, month)
+        let firstWeekDay = this.GetMonthFirstWeekDay(year, month)
+        let lastDayOfMonth = this.GetMonthLastDay(year, month)
+        let cycleNumbers = (lastDayOfMonth % 7) != 0 ? parseInt(String(lastDayOfMonth / 7)) + 1 : parseInt(String(lastDayOfMonth / 7));
+        let countDays = 0
+        let datesArr = []
+        for (let i = 0; i <= cycleNumbers; i++) {
+            let tempArr = []
+            for (let j = 0; j < 7; j++) {
+                let tempArrItem = {
+                    gregorian: '',
+                    lunar: '',
+                    monthDay: ''
+                }
+                if (i == 0) {
+                    //firstWeekDay为0 需要特殊处理
+                    if (firstWeekDay == 0) {
+                        if (j < 6) {
+                            tempArr.push("")
+                        } else {
+                            countDays++
+                            tempArrItem = this.handleSetDatesItem(tempArrItem, year, month, countDays)
+                            tempArr.push(tempArrItem)
+                        }
+                    } else {
+                        if (firstWeekDay <= j + 1) {
+                            countDays++
+                            tempArrItem = this.handleSetDatesItem(tempArrItem, year, month, countDays)
+                            tempArr.push(tempArrItem)
+                        } else {
+                            tempArr.push("")
+                        }
+                    }
+                }
+                else if (countDays < lastDayOfMonth) {
+                    countDays++
+                    tempArrItem = this.handleSetDatesItem(tempArrItem, year, month, countDays)
+                    tempArr.push(tempArrItem)
+                } else if (countDays >= lastDayOfMonth) {
+                    countDays++
+                    tempArr.push("")
+                }
+            }
+            datesArr.push(tempArr)
+        }
+        return datesArr
+    },
+    handleSetDatesItem(tempArrItem, year, month, countDays) {
+        let lunar = getLunar(year, month, countDays);
+        tempArrItem.gregorian = countDays
+        tempArrItem.monthDay = month + "/" + countDays
+        tempArrItem.lunar = lunar.dateStr.substr(-2, 2)
+        return tempArrItem
+    },
+
+    GetMonthFirstWeekDay(year, month) {
+        const firstDayOfMonth = new Date(year, month - 1, 1);
+        const dayOfWeek = firstDayOfMonth.getDay();
+        return dayOfWeek;
+    },
+    GetMonthLastDay(year, month) {
+        const lastDayOfMonth = new Date(year, month, 0).getDate();
+        return lastDayOfMonth
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad() {
+        this.setData({
+            today: new Date().getDate(),
+            monthNow: new Date().getMonth() + 1
+        })
+        this.setDatesOfYear()
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+        this.handleCountHolidayArr()
+        if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+            this.getTabBar().setData({
+                selected: 1
+            })
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
     }
-    return datesArr
-  },
-  handleSetDatesItem(tempArrItem, year, month, countDays) {
-    let lunar = getLunar(year, month, countDays);
-    tempArrItem.gregorian = countDays
-    tempArrItem.lunar = lunar.dateStr.substr(-2, 2)
-    return tempArrItem
-  },
-
-  GetMonthFirstWeekDay(year, month) {
-    const firstDayOfMonth = new Date(year, month - 1, 1);
-    const dayOfWeek = firstDayOfMonth.getDay();
-    return dayOfWeek;
-  },
-  GetMonthLastDay(year, month) {
-    const lastDayOfMonth = new Date(year, month, 0).getDate();
-    return lastDayOfMonth
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad() {
-    this.setData({
-      today: new Date().getDate(),
-      monthNow: new Date().getMonth() + 1
-    })
-    this.setDatesOfYear()
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
-      this.getTabBar().setData({
-        selected: 1
-      })
-    }
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
